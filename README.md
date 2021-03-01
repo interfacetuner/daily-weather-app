c4be2989e99093c1956a3e60df1566f2b5ac874c
Damian Hankins <damianchankins@gmail.com>
2021-02-28 20:09:14 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1><bold>Daily Weather App</bold></h1>
+
+
+<ul>
+<li>Enter your zip code</li>
+<li>Gets current local weather</li>
+<li>Provides temperature and wind speed</li>
+</ul>
+
+<h2>Technololgies used:</h2>
+
+<ul>
+<li>JavaScript</li>
+<li>jQuery</li>
+<li>CSS</li>
+<li>HTML</li>
+</ul>
+
+<h2>API Source:</h2>
+
+<h4>(https//:www.openweathermap.org)</h4>
---
+++ js/script.js
@@ -0,0 +1,41 @@
+// const API_KEY ="18a2aaf2";
+// const BASE_URL ="";
+
+// cached element refs/// selected dom elements
+const $weather = $(`#weather`);
+const $temp = $(`#temp`);
+const $wind = $(`#wind`);
+const $input = $(`input[type="text"]`);
+
+// event listeners
+$('form').on('submit', handleSubmit);
+
+// functions         -any event
+function handleSubmit(evt) {
+  evt.preventDefault(); //stop default browser from refresh
+
+  const term = $input.val();//user input
+      
+    $input.val(""); //remove user input
+
+    $.ajax("http://api.openweathermap.org/data/2.5/weather?zip=,&appid=5d103950c4eefff71dae1484657a90e6" + term)
+    .then(function(data) {
+    console.log('weather Data ', data);
+    weatherData = data;
+    render();
+  }, function(error) {
+    alert('Error ', error);
+  })
+
+};
+// update page 
+function render() {
+  if (weatherData) {
+    $weather.text(weatherData.name);
+    $temp.text(weatherData.main.temp);
+    $wind.text(weatherData.wind.speed);
+  }
+}
+
+// zip=08234
+// zip=08401,
